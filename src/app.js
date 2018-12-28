7b72092edacfcb139110dce42e8d44d5d7095736
rkgallaway <40313258+rkgallaway@users.noreply.github.com>
2018-12-27 16:25:08 -0800

    design-06 assignment
    basic changes made as per assignment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import Header from './components/header/header.js';
-import Footer from './components/footer/footer.js';
-import Content from './components/content/content.js';
+import React from "react";
+import Header from "./components/header/header.js";
+import Footer from "./components/footer/footer.js";
+import Content from "./components/content/content.js";
+import "./design/reset.scss";
+import "./design/design.scss";
 
 const App = () => {
   return (
