162ed82c9751de690fc4ffec917f36495a6d04aa
rkgallaway <40313258+rkgallaway@users.noreply.github.com>
2019-01-08 08:40:43 -0800

    design 11
    not 100% done but getting started

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Header = props => {
   return (
     <header>
       <h1>{props.title}</h1>
       {props.children}
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <li>Info</li>
+        </ul>
+      </nav>
     </header>
   );
 };
